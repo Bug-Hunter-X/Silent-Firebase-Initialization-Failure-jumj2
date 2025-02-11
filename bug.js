e1534cde8eddfd849cbd25f1a01f548d6da3ed53
BugHunter-X <askzenai@gmail.com>
2025-02-11 11:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// import other necessary Firebase services
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ... rest of the Firebase code ...
